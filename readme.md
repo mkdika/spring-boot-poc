da90f32728920f8d86accf3542e618046b6f0106
Maikel Chandika <mkdika@gmail.com>
2018-02-13 08:33:52 +0700

    first init & add spring-hibernate-association project

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Spring Boot POC
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](/LICENSE)
+
+A repository for Spring Boot Project Snippets, Example, Proof of Concept. 
+Especially in order to host my [blog](http://blog.mkdika.com) post article 
+source code related to [Spring Framework](https://spring.io/).
---
+++ spring-hibernate-association/src/main/java/com/mkdika/springbootpoc/springhibernateassociation/SpringHibernateAssociationApplication.java
@@ -0,0 +1,12 @@
+package com.mkdika.springbootpoc.springhibernateassociation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHibernateAssociationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHibernateAssociationApplication.class, args);
+	}
+}
